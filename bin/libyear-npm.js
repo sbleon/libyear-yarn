9f5a3e1d75799600e19c044b815077e53e12ca06
Jared Beck <jared@jaredbeck.com>
2017-03-05 02:07:20 -0500

    Release dates via npm view

--- bin/libyear-npm.js
+++ bin/libyear-npm.js
@@ -6,6 +6,7 @@ var exec = require('child_process').exec;
 var cmd = 'npm outdated --json';
 var _ = require('lodash');
 var vsprintf = require("sprintf-js").vsprintf;
+var execSync = require('child_process').execSync;
 
 exec(cmd, function(error, stdout, stderr) {
   // Just in case they don't actually have npm on their path, which would
@@ -26,9 +27,35 @@ exec(cmd, function(error, stdout, stderr) {
   }
 
   var parsed = JSON.parse(stdout);
+
+  var releaseTime = function(packageName, version) {
+    var cmd = 'npm view --json ' + packageName;
+    var stdout = null;
+    try {
+      stdout = execSync(cmd);
+    } catch(e) {
+      process.stderr.write('Failed to run: ' + cmd + '\n');
+      process.stderr.write(error.message);
+      process.exit(1);
+    }
+    if (stdout === '') {
+      process.stderr.write('npm view produced no output, no idea why');
+      process.exit(1);
+    }
+    var parsed = JSON.parse(stdout);
+    return _.get(parsed, ['time', version]);
+  };
+
   var report = [];
   _.forEach(parsed, function(value, key) {
-    var row = vsprintf("%20s %10s %10s", [key, value['current'], value['latest']]);
+    var currentVersion = value['current'];
+    var currentTime = releaseTime(key, currentVersion);
+    var latestVersion = value['latest'];
+    var latestTime = releaseTime(key, latestVersion);
+    var row = vsprintf(
+      "%20s %10s %30s %10s %30s",
+      [key, currentVersion, currentTime, latestVersion, latestTime]
+    );
     process.stdout.write(row + '\n');
   });
 });
