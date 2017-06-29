dcc57580be29234a3270184d7b5bdda1aec85e99
Jared Beck <jared@jaredbeck.com>
2017-06-29 18:49:20 -0400

    First unit test

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -1,7 +1,22 @@
 describe("calculator", function() {
   var calculator = require('../lib/calculator.js');
 
-  it("is a function", function () {
-    expect(typeof(calculator)).toEqual("function")
+  it("is a function", function() {
+    expect(typeof(calculator)).toEqual("function");
+  });
+
+  describe("nothing is outdated", function() {
+    it("prints a total of 0.0", function() {
+      var outdated = '';
+      var releaseTime = function() {
+        throw "test failed: unexpected call"
+      };
+      var stdout = { write: function() {} };
+      spyOn(stdout, 'write');
+      calculator(outdated, releaseTime, stdout);
+      expect(stdout.write).toHaveBeenCalledWith(
+        'System is 0.0 libyears behind\n'
+      );
+    });
   });
 });
