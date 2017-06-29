2f20d9acc69a831c18a58107da79423045a4242b
Jared Beck <jared@jaredbeck.com>
2017-06-29 19:01:00 -0400

    Another actual test :tada:
    super heavy stubbing, but it's a real test with real value

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -1,5 +1,6 @@
 describe("calculator", function() {
   var calculator = require('../lib/calculator.js');
+  var moment = require('moment');
 
   it("is a function", function() {
     expect(typeof(calculator)).toEqual("function");
@@ -17,6 +18,36 @@ describe("calculator", function() {
       expect(stdout.write).toHaveBeenCalledWith(
         'System is 0.0 libyears behind\n'
       );
+    });
+  });
+
+  describe("a package is outdated", function() {
+    it("prints the expected output", function() {
+      var outdated = JSON.stringify(
+        {
+          "banana": {
+            "current": "1.0.0",
+            "wanted": "1.0.1",
+            "latest": "2.0.0",
+            "location": "node_modules/banana"
+          }
+        }
+      );
+      var releaseTime = function(packageName, version) {
+        return {
+          "1.0.0": moment("2016-02-28"),
+          "1.0.1": moment("2016-02-29"),
+          "2.0.0": moment("2017-02-28")
+        }[version];
+      };
+      var stdout = { write: function() {} };
+      spyOn(stdout, 'write');
+      calculator(outdated, releaseTime, stdout);
+      expect(stdout.write).toHaveBeenCalledWith(
+        '                        banana      1.0.0      2016-02-28' +
+        '      2.0.0      2017-02-28    1.00\n'
+      );
+      expect(stdout.write).toHaveBeenCalledWith('System is 1.0 libyears behind\n');
     });
   });
 });
